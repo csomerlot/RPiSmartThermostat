486d1072d8a7d63eab0fd26f587e247ef85a78d3
Chris Somerlot <csomerlot@brwncald.com>
2015-10-06 20:06:44 -0400

    switched wifi AP to production

--- src/ESP8266_RelayServer/ESP8266_RelayServer.ino
+++ src/ESP8266_RelayServer/ESP8266_RelayServer.ino
@@ -3,7 +3,7 @@
 #include <ESP8266WebServer.h>
 #include <ESP8266mDNS.h>
 
-const char* ssid = "linksys";
+const char* ssid = "raspberry";
 const char* password = "somerlotsomerlot";
 MDNSResponder mdns;
 ESP8266WebServer server(80);

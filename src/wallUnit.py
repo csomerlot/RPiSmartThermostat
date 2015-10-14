7d16640166c390c01225c2ae7277d318c01ec000
Chris Somerlot <csomerlot@brwncald.com>
2015-10-13 22:03:54 -0400

    added messaging

--- src/wallUnit.py
+++ src/wallUnit.py
@@ -1,4 +1,8 @@
+#!/usr/bin/env python
+
 import sys, time, urllib, datetime
+
+import Adafruit_CharLCD as LCD
 
 ##sys.path.append(r'..\libs\Adafruit_Python_DHT')
 ##sys.path.append(r'..\libs\Adafruit_Python_CharLCD')
@@ -7,8 +11,8 @@ import sys, time, urllib, datetime
 ##sensor = Adafruit_DHT.DHT22
 ##pin = 4
 
-from pymongo import MongoClient
-mongo_client = MongoClient()
+##from pymongo import MongoClient
+##mongo_client = MongoClient()
 
 def getTarget():
     return 68
@@ -19,22 +23,52 @@ def getTemp():
     return 62
 
 def main():
+    lcd = LCD.Adafruit_CharLCDPlate()
+    lcd.set_color(1,1,1)
+    
+    messages = ['RPi Thermostat']
+    messages.append(getTime())
+    message.append("IP %s" % getIp())
+    mindx = 1
+    lcd.message(messages[mindx])
+    
     while True:
         
-
-        mongo_client['HomeControl'].temperature.drop ()
-        mongo_client['HomeControl']['temperature'].insert(temperature)
-
-        if temperature < getTarget:
-            urllib.urlopen(r'http://192.168.42.130/relay2On')
-        else:
-            urllib.urlopen(r'http://192.168.42.130/relay2On')
+##        mongo_client['HomeControl'].temperature.drop ()
+##        mongo_client['HomeControl']['temperature'].insert(temperature)
+##
+##        if temperature < getTarget:
+##            urllib.urlopen(r'http://192.168.42.130/relay2On')
+##        else:
+##            urllib.urlopen(r'http://192.168.42.130/relay2On')
                 
-        time.sleep(60)
+        if lcd.is_pressed(LCD.UP):
+            mindx -= 1
+            if mindx == 0:
+                mindx = len(midx)-1
 
+            lcd.clear()
+            lcd.message(messages[mindx])
+            
+        if lcd.is_pressed(LCD.DOWN):
+            mindx += 1
+            if mindx == len(midx)-1:
+                mindx = 0
+
+            lcd.clear()
+            lcd.message(messages[mindx])
+
+def getIp():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.connect(("8.8.8.8",80))
+    return s.getsockname()[0]
+
+def getTime():
+    d = datetime.datetime.now()
+    return d.strftime("%m/%d %I:%M")
 
 if __name__ == '__main__':
-
+    
     ## check internet connection
     
     ## check for relay module

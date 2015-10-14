69e271ff41e4b5d6298b264dbafaea5f4986d4b0
Chris Somerlot <csomerlot@brwncald.com>
2015-10-13 21:37:22 -0400

    reorg structure

--- src/wallUnit.py
+++ src/wallUnit.py
@@ -1,11 +1,11 @@
-import sys, time, urllib2, datetime
+import sys, time, urllib, datetime
 
-sys.path.append(r'..\libs\Adafruit_Python_DHT')
-sys.path.append(r'..\libs\Adafruit_Python_CharLCD')
-
-import Adafruit_DHT
-sensor = Adafruit_DHT.DHT22
-pin = 4
+##sys.path.append(r'..\libs\Adafruit_Python_DHT')
+##sys.path.append(r'..\libs\Adafruit_Python_CharLCD')
+##
+##import Adafruit_DHT
+##sensor = Adafruit_DHT.DHT22
+##pin = 4
 
 from pymongo import MongoClient
 mongo_client = MongoClient()
@@ -13,16 +13,31 @@ mongo_client = MongoClient()
 def getTarget():
     return 68
 
-while True:
-    humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
-    temperature = temperature * 9/5.0 + 32
+def getTemp():
+##    humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
+##    temperature = temperature * 9/5.0 + 32
+    return 62
 
-    mongo_client['HomeControl'].temperature.drop ()
-    mongo_client['HomeControl']['temperature'].insert(temperature)
+def main():
+    while True:
+        
 
-    if temperature < getTarget:
-        urllib2.urlopen(r'http:\\192.168.1.101\relay2On')
-    else:
-        urllib2.urlopen(r'http:\\192.168.1.101\relay2Off')
-            
-    time.sleep(60)
+        mongo_client['HomeControl'].temperature.drop ()
+        mongo_client['HomeControl']['temperature'].insert(temperature)
+
+        if temperature < getTarget:
+            urllib.urlopen(r'http://192.168.42.130/relay2On')
+        else:
+            urllib.urlopen(r'http://192.168.42.130/relay2On')
+                
+        time.sleep(60)
+
+
+if __name__ == '__main__':
+
+    ## check internet connection
+    
+    ## check for relay module
+
+    ## get local weather
+    
